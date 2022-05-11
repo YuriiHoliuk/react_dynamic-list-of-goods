6811fe54f6bb9116035f814f080c5f7114eb99f8
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-05-11 11:58:08 +0300

    refactored

---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,65 @@
+import { FC } from 'react';
+import { Good } from '../typedefs';
+
+interface Props {
+  goods: Good[];
+  onToggle: (id: number) => void;
+  onRename: (id: number, name: string) => void;
+  onAdd: () => void;
+  onRemove: (id: number) => void;
+}
+
+export const GoodsList: FC<Props> = (props) => {
+  const {
+    goods,
+    onToggle,
+    onRename,
+    onAdd,
+    onRemove,
+  } = props;
+
+  return (
+    <>
+      <ul>
+        {goods.map((good) => (
+          <li
+            key={good.id}
+            className={good.isActive ? 'active' : ''}
+            style={{ marginBottom: '15px' }}
+          >
+            <input
+              type="text"
+              value={good.name}
+              onChange={(event) => (
+                onRename(good.id, event.target.value)
+              )}
+            />
+
+            <button
+              type="button"
+              onClick={() => onToggle(good.id)}
+              style={{ marginLeft: '10px' }}
+            >
+              Toggle
+            </button>
+
+            <button
+              type="button"
+              onClick={() => onRemove(good.id)}
+              style={{ marginLeft: '10px' }}
+            >
+              🗑
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button
+        type="button"
+        onClick={onAdd}
+      >
+        Add new good
+      </button>
+    </>
+  );
+};
